43f4e978d3984e81b5eaba75cd1e4f773bfcd2e0
olivepierre <35138826+olivepierre@users.noreply.github.com>
2019-04-15 18:02:09 +0200

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,9 @@ set -e
 git config --global user.email "olivepierre@gmail.com"
 git config --global user.name "Pierre Olive"
 
-git clone https://${TRAVIS_CI_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone --single-branch --branch master https://${TRAVIS_CI_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../docs/* ./
-git add --all *
+git add .
 git commit -m "Reconstruction du livre" || true
-git push -q origin gh-pages
+git push origin gh-pages
